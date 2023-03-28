91ed1aee533f5ed4ea327195a36f3fa3103b9cc8
0x7266 <rrrr0ffff@gmail.com>
2023-03-28 00:14:40 -0300

    fixed images

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,23 +3,25 @@ import product from "../variants/product";
 import productsList from "../variants/productsList";
 
 export default function Product({ p, index }) {
-  return (
-    <motion.div
-      className="flex flex-col items-center sm:items-stretch sm:flex-row gap-3"
-      // variants={productsList}
-      initial="hidden"
-      animate="show"
-    >
-      <motion.div
-        className="flex flex-col gap-3 w-64 bg-chrltt p-3 rounded text-xl max-h-96 font-bold"
-        variants={product(index)}
-      >
-        <img
-          src={`src/assets/${p.id}.jpg`}
-          className="w-full h-60 object-cover rounded"
-        />
-        <span>{p.name}</span>
-      </motion.div>
-    </motion.div>
-  );
+	return (
+		<motion.div
+			className="flex flex-col items-center sm:items-stretch sm:flex-row gap-3"
+			// variants={productsList}
+			initial="hidden"
+			whileInView="show"
+			viewport={{ once: true, amount: 0.4 }}
+		>
+			<motion.div
+				className="flex flex-col gap-3 w-64 bg-chrltt p-3 rounded text-xl max-h-96 font-bold"
+				variants={product(index)}
+			>
+				<img
+					// src={`https://production-darez-api.s3.amazonaws.com/products/1599053284028_IMAGE1599053283566.jpg`}
+					src={`assets/${p.id}.jpg`}
+					className="w-full h-60 object-cover rounded"
+				/>
+				<span>{p.name}</span>
+			</motion.div>
+		</motion.div>
+	);
 }
