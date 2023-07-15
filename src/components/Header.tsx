1963a7eb95620047229fa356d2b314b7722f97b1
unknown <rrrr0ffff@gmail.com>
2023-07-15 15:22:51 -0300

    animated navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,45 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { motion } from "framer-motion";
 import logo from "/images/logo.png";
 
 export default function Header() {
+	const location = useLocation();
+
+	const pages = [
+		["Produtos", "produtos"],
+		["Encomendas", "encomendas"],
+		["Localização", "localizacao"],
+	];
+	console.log(location.pathname);
+
 	return (
 		<header className="bg-chrltt flex flex-col justify-center items-center py-1 px-2 sm:px-10 pt-0 h-fit w-full">
 			<div className="container flex flex-col sm:flex-row justify-around items-center">
 				<Link to="/" className="focus:outline-red-400">
 					<img className="w-20" src={logo} />
 				</Link>
-				<nav className="flex w-full justify-between sm:w-fit sm:gap-10 text-neutral-200 font-bold">
-					<Link to="produtos">Produtos</Link>
-					<Link to="encomendas">Encomendas</Link>
-					<Link to="localizacao">Localização</Link>
+				<nav className="flex w-full justify-between items-center sm:w-fit sm:gap-10 font-bold">
+					{pages.map((page) => (
+						<Link
+							to={page[1]}
+							key={page[1]}
+							className={`relative px-3 py-2 transition-all ${
+								page[1] === location.pathname.slice(1)
+									? "text-chrltt"
+									: "text-white"
+							}`}
+						>
+							{page[1] === location.pathname.slice(1) ? (
+								<motion.div
+									transition={{ duration: 0.2 }}
+									layoutId="active"
+									className="absolute inset-0 bg-white"
+									style={{ borderRadius: 9999999 }}
+								/>
+							) : null}
+							<span className="relative z-20 ">{page[0]}</span>
+						</Link>
+					))}
 				</nav>
 			</div>
 		</header>
--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -2,12 +2,12 @@ import { motion } from "framer-motion";
 export default function Orders() {
 	return (
 		<div className="container flex flex-col items-center">
-			<div className="flex flex-col gap-10 py-2 px-5 sm:items-start md:w-11/12">
+			<div className="flex flex-col gap-10 py-2 px-5 w-full m:items-start md:w-11/12 bg-orange-300">
 				<motion.h2
 					initial={{ opacity: 0 }}
 					animate={{ opacity: 1 }}
 					transition={{ duration: 1 }}
-					className="text-5xl sm:text-6xl font-bold self-start"
+					className="text-5xl sm:text-6xl font-bold text-center"
 				>
 					Encomendas
 				</motion.h2>
@@ -20,21 +20,21 @@ export default function Orders() {
 						<a
 							href="https://delivery.yooga.app/charlotte-cake-shop"
 							target="_blank"
-							className="font-bold bg-white text-center rounded-md p-3 shadow-[5px_9px_0_rgba(255,70,69,255)] hover:translate-x-[5px] hover:translate-y-[9px] text-black ease-out transition-all hover:shadow-none duration-200 w-full sm:w-2/5 max-w-2xl"
+							className="font-bold bg-white text-center rounded-md px-3 py-5 shadow-[5px_9px_0_rgba(255,70,69,255)] hover:translate-x-[5px] hover:translate-y-[9px] text-black ease-out transition-all hover:shadow-none duration-200 w-full lg:w-4/5 max-w-2xl"
 						>
 							Delivery
 						</a>
 						<a
 							href="https://drive.google.com/file/d/1GDIqnYJ_Q59OkjaiMlsiMA9OFh-Sus-K/view?usp=drivesdk"
 							target="_blank"
-							className="font-bold bg-white text-center rounded-md p-3 shadow-[5px_9px_0_rgba(255,70,69,255)] hover:translate-x-[5px] hover:translate-y-[9px] text-black ease-out transition-all hover:shadow-none duration-200 w-full sm:w-2/5 max-w-2xl"
+							className="font-bold bg-white text-center rounded-md px-3 py-5 shadow-[5px_9px_0_rgba(255,70,69,255)] hover:translate-x-[5px] hover:translate-y-[9px] text-black ease-out transition-all hover:shadow-none duration-200 w-full sm:w-4/5 max-w-2xl"
 						>
 							Cardápio de Páscoa
 						</a>
 						<a
 							href="https://wa.me/message/IKQHQOIETDGZG1"
 							target="_blank"
-							className="font-bold bg-white text-center rounded-md p-3 shadow-[5px_9px_0_rgba(255,70,69,255)] hover:translate-x-[5px] hover:translate-y-[9px] text-black ease-out transition-all hover:shadow-none duration-200 w-full sm:w-2/5 max-w-2xl"
+							className="font-bold bg-white text-center rounded-md px-3 py-5 shadow-[5px_9px_0_rgba(255,70,69,255)] hover:translate-x-[5px] hover:translate-y-[9px] text-black ease-out transition-all hover:shadow-none duration-200 w-full sm:w-4/5 max-w-2xl"
 						>
 							Whatsapp
 						</a>
