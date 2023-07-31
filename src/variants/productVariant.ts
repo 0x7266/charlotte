e7c451ed2f7f6f58194239363073bf07e16279ed
unknown <rrrr0ffff@gmail.com>
2023-07-30 23:25:04 -0300

    changed products apage animation

--- src/variants/productVariant.ts
+++ src/variants/productVariant.ts
@@ -5,16 +5,15 @@ export const productVariants = {
 	animate: (index: number) => ({
 		opacity: 1,
 		transition: {
-			delay: index * 0.1,
-			when: "beforeChildren",
-			delayChildren: 0.2,
-			staggerChildren: 0.2,
+			delay: index * 0.15,
+			delayChildren: index * 0.2,
+			// staggerChildren: 0.1,
 		},
 	}),
 	exit: (index: number) => ({
 		opacity: 0,
 		transition: {
-			delay: index * 0.1,
+			delay: index * 2,
 			staggerDirection: -1,
 		},
 	}),
@@ -22,10 +21,10 @@ export const productVariants = {
 
 export const productImageVariants = {
 	initial: { y: -300 },
-	animate: { y: 0 },
+	animate: { y: 0, transition: { type: "spring", bounce: 0.2 } },
 };
 
 export const productNameVariants = {
 	initial: { y: 100 },
-	animate: { y: 0 },
+	animate: { y: 0, transition: { type: "spring", bounce: 0.2 } },
 };
