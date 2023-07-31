0cadf1ebd71710c5de03f984fc6c0c7e9da48981
unknown <rrrr0ffff@gmail.com>
2023-07-31 00:52:07 -0300

    added header animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,47 +1,105 @@
 import { Link, useLocation } from "react-router-dom";
-import { motion } from "framer-motion";
+import { Variants, motion } from "framer-motion";
 import logo from "/images/logo.png";
+import { useState } from "react";
 
 export default function Header() {
 	const location = useLocation();
+	const [active, setActive] = useState(location.pathname.slice(1));
 
 	const pages = [
 		["Produtos", "produtos"],
 		["Encomendas", "encomendas"],
 		["Localização", "localizacao"],
 	];
-	console.log(location.pathname);
+
+	const containerVariants: Variants = {
+		initial: {
+			y: -100,
+		},
+		animate: {
+			y: 0,
+			transition: {
+				type: "spring",
+				bounce: 0,
+				duration: 0.4,
+				when: "beforeChildren",
+				staggerChildren: 0.1,
+			},
+		},
+	};
+
+	const elementVariants: Variants = {
+		initial: {
+			y: -100,
+		},
+		animate: {
+			y: 0,
+			transition: {
+				type: "spring",
+				bounce: 0.3,
+				duration: 0.4,
+				when: "beforeChildren",
+				staggerChildren: 0.1,
+			},
+		},
+	};
+
+	const linkVariants: Variants = {
+		initial: {
+			y: -100,
+		},
+		animate: {
+			y: 0,
+			transition: {
+				type: "spring",
+				bounce: 0.3,
+				duration: 0.4,
+			},
+		},
+	};
 
 	return (
 		<header className="bg-chrltt flex flex-col justify-center items-center py-1 px-2 sm:px-10 pt-0 h-fit w-full">
-			<div className="container flex flex-col sm:flex-row justify-around items-center">
-				<Link to="/" className="focus:outline-red-400">
-					<img className="w-20" src={logo} />
-				</Link>
-				<nav className="flex w-full justify-between items-center sm:w-fit sm:gap-10 font-bold">
+			<motion.div
+				variants={containerVariants}
+				initial="initial"
+				animate="animate"
+				className="container flex flex-col sm:flex-row justify-around items-center"
+			>
+				<motion.div variants={elementVariants}>
+					<Link to="/" className="focus:outline-red-400">
+						<img className="w-20" src={logo} />
+					</Link>
+				</motion.div>
+				<motion.nav
+					variants={elementVariants}
+					className="flex w-full justify-between items-center sm:w-fit sm:gap-10 font-bold text-[#2ACFC8]"
+				>
 					{pages.map((page) => (
-						<Link
-							to={page[1]}
-							key={page[1]}
-							className={`relative px-3 py-2 transition-all ${
-								page[1] === location.pathname.slice(1)
-									? "text-chrltt"
-									: "text-white"
-							}`}
-						>
-							{page[1] === location.pathname.slice(1) ? (
-								<motion.div
-									transition={{ duration: 0.2 }}
-									layoutId="active"
-									className="absolute inset-0 bg-white"
-									style={{ borderRadius: 6 }}
-								/>
-							) : null}
-							<span className="relative z-20 ">{page[0]}</span>
-						</Link>
+						<motion.div variants={linkVariants}>
+							<Link
+								to={page[1]}
+								key={page[1]}
+								onClick={() => setActive(page[1])}
+								className={`relative px-3 py-2 transition-all `}
+							>
+								{page[1] === active && (
+									<motion.div
+										layoutId="active"
+										transition={{ type: "spring", duration: 1 }}
+										className="absolute inset-0 bg-white"
+										style={{ borderRadius: 6 }}
+									/>
+								)}
+								<span className="relative z-20 mix-blend-exclusion bg-blend-exclusion">
+									{page[0]}
+								</span>
+							</Link>
+						</motion.div>
 					))}
-				</nav>
-			</div>
+				</motion.nav>
+			</motion.div>
 		</header>
 	);
 }
