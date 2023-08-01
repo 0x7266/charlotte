44d7bb7f6b760f0364c783673902a0f389d875f5
unknown <rrrr0ffff@gmail.com>
2023-08-01 11:19:21 -0300

    removed dead code

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -6,19 +6,17 @@ import Product from "../components/Product";
 export default function Products() {
 	return (
 		<div className="container flex flex-col items-center">
-			<div className="flex flex-col justify-center h-full sm:items-start gap-10 py-2 px-5  md:w-11/12">
-				<motion.div
-					variants={productsContainerVariants}
-					initial="initial"
-					animate="animate"
-					exit="exit"
-					className="flex flex-col flex-wrap items-center sm:items-stretch sm:flex-row gap-3"
-				>
-					{products.map((product, index) => (
-						<Product key={index} product={product} />
-					))}
-				</motion.div>
-			</div>
+			<motion.div
+				variants={productsContainerVariants}
+				initial="initial"
+				animate="animate"
+				exit="exit"
+				className="flex flex-col flex-wrap items-center sm:items-stretch sm:flex-row gap-3"
+			>
+				{products.map((product, index) => (
+					<Product key={index} product={product} />
+				))}
+			</motion.div>
 		</div>
 	);
 }
